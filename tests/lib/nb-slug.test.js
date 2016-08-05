f321693cc2b2a4a20126dc1dfb6b6874de9895d4
Mehmet Baker <mehmet.baker@gmail.com>
2016-08-05 13:58:59 +0300

    New tests added
    Signed-off-by: Mehmet Baker <mehmet.baker@gmail.com>

--- tests/lib/nb-slug.test.js
+++ tests/lib/nb-slug.test.js
@@ -1,56 +1,74 @@
 var nbSlugPath = '../../lib/nb-slug';
+var dictPath = '../../lib/diacritics';
 jest.dontMock(nbSlugPath);
+jest.dontMock(dictPath);
 
-describe('nbSlug', function() {
+describe('nbSlug', function () {
   var nbSlug;
 
-  beforeEach(function() {
+  beforeEach(function () {
     nbSlug = require(nbSlugPath);
   });
 
-  it ('should is defined', function() {
+  it('should is defined', function () {
     expect(nbSlug).toBeDefined();
   });
 
-  it ('should replace blank spaces', function() {
+  it('should replace blank spaces', function () {
     var slug = nbSlug('first                  string');
     var expected = 'first-string';
-    expect(nbSlug).toBeDefined(nbSlug);
+    expect(slug).toBe(expected);
   });
 
-  it ('should replace scores', function() {
-    var slug = nbSlug('second --- string');
+  it('should replace scores', function () {
     var expected = 'second-string';
-    expect(nbSlug).toBeDefined(expected);
+    var scoreStr = [
+      'second-string',
+      'second--string',
+      'second---string',
+      'second----string',
+      'second-----string',
+      'second------string'
+    ];
+
+    for (var i = 0; i < scoreStr.length; ++i) {
+      var slug = nbSlug(scoreStr[i]);
+
+      expect(slug).toBe(expected);
+    }
   });
 
-  it ('should replace accents', function() {
+  it('should replace accents', function () {
     var slug = nbSlug('other áéíóúńñãõçâîôû string');
     var expected = 'other-aeiounnaocaiou-string';
-    expect(nbSlug).toBeDefined(expected);
+    expect(slug).toBe(expected);
   });
 
-  it ('should replace underscore', function() {
+  it('should replace underscore', function () {
     var slug = nbSlug('other_string');
-    var expected = 'other-aeiounnaocaiou-string';
-    expect(nbSlug).toBeDefined(expected);
+    var expected = 'other-string';
+    expect(slug).toBe(expected);
   });
 
-  it ('should ignore last trance', function() {
+  it('should ignore last trance', function () {
     var slug = nbSlug('other  string -');
     var expected = 'other-string';
-    expect(nbSlug).toBeDefined(expected);
+    expect(slug).toEqual(expected);
   });
 
-  it ('should replace to lowercase', function() {
-    var slug = nbSlug('OTHER ÁÉÍÓÚŃÑÃÕÇÂÎÔÛ STRING');
-    var expected = 'other-aeiounnaocaiou-string';
-    expect(nbSlug).toBeDefined(expected);
+  it('should ignore first trance', function () {
+    var slug = nbSlug('- other  string');
+    var expected = 'other-string';
+    expect(slug).toBe(expected);
   });
 
-  it ('should ignore accents', function() {
+  it('should replace to lowercase', function () {
+    expect(nbSlug('OTHER ÁÉÍÓÚŃÑÃÕÇÂÎÔÛ STRING')).toBe('other-aeiounnaocaiou-string');
+  });
+
+  it('should ignore accents', function () {
     var slug = nbSlug('MY STRING ÁÉÍÓ_ al}^ ?}^{    ((    ))}    dirty and  now ÚŃÑÃÕÇÂÎÔÛ STRING clear');
-    var expected = 'my-string-aeiou-al-dirty-and-now-unnaoçaiou-string-clear';
-    expect(nbSlug).toBeDefined(expected);
+    var expected = 'my-string-aeio-al-dirty-and-now-unnaocaiou-string-clear';
+    expect(slug).toBe(expected);
   });
 });
